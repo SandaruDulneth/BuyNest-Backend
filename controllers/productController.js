1954feac067832bb6005922335ac120680beb58c
SenalAbeysekara <senaldushyantha@gmail.com>
2025-09-10 19:55:15 +0530

    Product Update

--- controllers/productController.js
+++ controllers/productController.js
@@ -57,6 +57,18 @@ export async function getProducts(req, res) {
     }
 }
 
+export const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
 export async function getProductsByCategory(req, res) {
     try {
         const category = req.body.category;
