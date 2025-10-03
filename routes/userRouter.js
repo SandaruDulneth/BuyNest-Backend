07f16c6c912cf3bbc5c4e7e90e00103e57ace3fd
Sandaru Dulneth <sdulneth29@gmail.com>
2025-10-03 18:15:07 +0530

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,1 @@
-import express from "express";
-import { createUser, loginUser } from "../controllers/userController.js";
 
-const userRouter = express.Router();
-
-userRouter.post("/",createUser);
-userRouter.post("/login",loginUser);
-
-
-
-
-
-export default userRouter;
